292472ce38937c752f070e1ba62e726f88394e46
Demopans <gao.jeff3@gmail.com>
2021-10-27 18:40:29 -0400

    can access firebase and that api

--- core/db/characterDB/charDB.py
+++ core/db/characterDB/charDB.py
@@ -1,6 +1,7 @@
 import requests
 import firebase_admin
 from firebase_admin import credentials, firestore
+from core.character.character import Character
 
 # Use the application default credentials
 from firebase_admin.auth import Client
@@ -21,7 +22,7 @@ class DB:
         if cls.instance is None:
             cls.instance = super(DB, cls).__new__(cls)
             # Put any initialization here.
-            cls.cred = credentials.Certificate('DBKeys/pyfagenshindb-firebase-adminsdk-nnt83-30c34bd520.json')
+            cls.cred = credentials.Certificate('../DBKeys/pyfagenshindb-firebase-adminsdk-nnt83-30c34bd520.json')
             firebase_admin.initialize_app(cls.cred, {'projectId': 'pyfagenshindb', })
             cls.db: Client = firestore.client()
 
@@ -30,6 +31,25 @@ class DB:
     def accessArtifacts(self):
         root: DocumentReference = self.db.document(u'Items/Artifacts')
         rt: list[str] = ['Crown', 'Cup', 'Feather', 'Flower', 'Watch']
+
+    def getAllChar(self):
+        r: requests.Response = requests.get(db1)
+        try:
+            r.raise_for_status()
+            # Code here will only run if the request is successful, skip updating db if api call fails
+            # build character db
+            tmp = dict()
+            for i in r.json():
+                tmp[i] = requests.get(db1 + i).content
+            return tmp
+
+        except Exception as err:
+            print(err)
+            return
+
+
+    def accessCharacterData(self, char:Character):
+        root :DocumentReference = self.db.document(u'Character/{0}'.format(char.name))
 
 
 # get char data not covered by firestore
@@ -60,3 +80,8 @@ def updateChars():
 
 def getStats(name: str, lv: int) -> tuple[int, int, int]:  # hp, atk, def
     pass
+
+
+if __name__ == '__main__':
+    dbref = DB.__new__(DB) # call first
+    dbref
