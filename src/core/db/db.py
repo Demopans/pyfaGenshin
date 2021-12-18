c815edb7419ad2bcc7b92361c52b992ee399980b
Demopans <gao.jeff3@gmail.com>
2021-12-18 15:08:14 -0500

    Refactored project. Moved autocheckin to a different repo

--- src/core/db/db.py
+++ src/core/db/db.py
@@ -1,31 +1,7 @@
-from genshinstats import *
-import requests
+class DBInterface:
+    root:str
 
-from src.core.db.characterDB.charDB import updateChars
-
-root: str = "http://api.genshin.dev"
-
-
-def dbNeedsUpdate():
-    # get version header of db, and compare with that of the api
-    # db only has the data required for fitting
-    pass
+    def connect(self):
+        pass
 
 
-def updateDB() -> None:
-    def updateWeaps():
-        r: requests.Response = requests.get(root + "/weapons")
-        try:
-            r.raise_for_status()
-            # Code here will only run if the request is successful, skip updating db if api call fails
-            # build character db
-            r.json()
-
-        except IOError as err:
-            print(err)
-            return
-
-    updateChars()
-    updateWeaps()
-
-    pass
